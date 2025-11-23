ca38981b32b1090d455c26b33b6f4325b7cf7ce0
tob-david <gerardusdavidbayu@gmail.com>
2025-11-23 23:26:54 +0700

    initial coomit

---
+++ Frontend/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div className="bg-red-500">
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
