a385dd0febf4d509c67ab7da9337ef482f03f4ba
tob-david <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2026-03-04 10:07:12 +0700

    feat: update layout and home component

--- Frontend/src/components/home/index.tsx
+++ Frontend/src/components/home/index.tsx
@@ -1,5 +1,15 @@
+import PaymentTimeline from "./PaymentTimeline";
+import PremiumSummary from "./PremiumSummary";
+import SupportSection from "./SupportSection";
+
 const HomeSection = () => {
-  return <div className="bg-red-500 h-full w-full">HomeSection</div>;
+  return (
+    <div className="bg-red-500 h-full w-full">
+      <PremiumSummary />
+      <PaymentTimeline />
+      <SupportSection />
+    </div>
+  );
 };
 
 export default HomeSection;
