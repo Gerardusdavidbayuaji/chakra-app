672d3f25cb3ffb1c0c22703d018d0bbd324f6102
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2026-03-18 20:22:49 +0700

    feat: update ApplicationDbContext.cs

---
+++ Backend/ChakraApp.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace  ChakraApp.Infrastruture.ApplicationDbContext;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    } 
+    //public DbSet<Premi> Premis {get; set;}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
+
